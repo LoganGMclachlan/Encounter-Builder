245b5c8793080666a531c2eb994d6f54c32a591c
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2023-09-07 14:39:14 +0100

    created login and register forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleLogin(e){
+        e.preventDefault()
+        alert(`email:${email}, password:${password}`)
+    }
+
     return(
     <>
-        <h2>Login</h2>
+        <div className="tab-container">
+            <div className="tab selected">Login</div>
+            <Link to="/register" className="tab">Register</Link>
+        </div>
+
+        <form onSubmit={handleLogin} className="form">
+            <input
+                placeholder="Email..."
+                className="form-input"
+                onChange={e => setEmail(e.target.value)}
+            />
+            <input
+                placeholder="Password..."
+                className="form-input"
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button className="blue-btn" type="submit">Login!</button>
+        </form>
+        
+        <div>
+        <h3>or</h3>
+        <button className="google-btn">Login with Google</button>
+        </div>
     </>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function Register() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    function handleRegister(e){
+        e.preventDefault()
+        alert(`email:${email}, password:${password}, confirm password:${confirmPassword}`)
+    }
+
     return(
     <>
-        <h2>Register</h2>
+        <div className="tab-container">
+            <Link to="/" className="tab ">Login</Link>
+            <div className="tab selected">Register</div>
+        </div>
+
+        <form onSubmit={handleRegister} className="form">
+            <input
+                placeholder="Email..."
+                className="form-input"
+                onChange={e => setEmail(e.target.value)}
+            />
+            <input
+                placeholder="Password..."
+                className="form-input"
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
+            <input
+                placeholder="Confirm Password..."
+                className="form-input"
+                type="password"
+                onChange={e => setConfirmPassword(e.target.value)}
+            />
+            <button className="blue-btn" type="submit">Register!</button>
+        </form>
+        
+        <div>
+        <h3>or</h3>
+        <button className="google-btn">Register with Google</button>
+        </div>
     </>
     )
 }
